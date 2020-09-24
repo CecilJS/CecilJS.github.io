b92b7f875e1e0f62ab7d7d9b951f33ccbc695552
Cecil Jones <dewdrops1800@outlook.com>
2020-09-24 23:57:58 +0100

    expertise updated

--- src/components/Expertise.js
+++ src/components/Expertise.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import js from './images/JS.jpg';
 import rc from './images/react.jpg';
 import node from './images/node.png';
+import api from './images/api.jpg';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
@@ -28,31 +29,31 @@ class Expertise extends Component {
                 <div className="carousel-inner">
                     <div className="carousel-item active">
                     <img src={js} className="d-block w-100" alt="Javascript"/>
-                    <div className="carousel-caption d-md-block">
+                    <div className="carousel-caption d-md-block cover" >
                         <h5>Language of choice - Javascript</h5>
-                        <p>As you will no doubt agree, a programming language is just tool computer programmers use to accomplish any programmable tasks along with other tools ofcourse. 
-                        As an a sofware, I am comfortable with other programming languages like Python and C#, however, my programming lannguage of choice is JavaScript.</p>
+                        <p> As a sofware engineer, I am comfortable with other programming languages like Python and C#, however, my programming lannguage of choice is JavaScript.</p>
+                            
                     </div>
                     </div>
                     <div className="carousel-item">
                     <img src={rc} className="d-block w-100" alt="React JS"/>
-                    <div className="carousel-caption d-md-block d-sm-block">
+                    <div className="carousel-caption d-md-block d-sm-block cover">
                         <h5>Frontend Development - React JS</h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <p>There is nothing better than being able to seperate your concerns while building complex user interfaces. I love React!</p>
                     </div>
                     </div>
                     <div className="carousel-item">
-                    <img src={rc} className="d-block w-100" alt="React JS"/>
-                    <div className="carousel-caption d-md-block d-sm-block">
-                        <h5>DB Development - React JS</h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <img src={api} className="d-block w-100" alt="React JS"/>
+                    <div className="carousel-caption d-md-block d-sm-block cover">
+                        <h5>Appliaction Programming Interface -API </h5>
+                        <p>What will we do without APIs? I am comfortable with RESTful APIs. I can equally work with SOAP APIs when needed.</p>
                     </div>
                     </div>
                     <div className="carousel-item">
                     <img src={node} className="d-block w-100" alt="Node JS"/>
-                    <div className="carousel-caption d-md-block">
+                    <div className="carousel-caption d-md-block cover">
                         <h5>Backend Development - Node JS</h5>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                        <p> I believe the beauty of JavaScript can be in seen our ability to use it from the Frontend to the Backend. I love JavaScript & the Node environment. </p>
                     </div>
                     </div>
                 </div>
