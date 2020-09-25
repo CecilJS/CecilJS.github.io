8d0bd1ae92d631c534f05bc9566fd4c2300e1e6e
Cecil Jones <dewdrops1800@outlook.com>
2020-09-25 01:21:09 +0100

    reverted to old styles

--- src/components/Expertise.js
+++ src/components/Expertise.js
@@ -29,7 +29,7 @@ class Expertise extends Component {
                 <div className="carousel-inner">
                     <div className="carousel-item active">
                     <img src={js} className="d-block w-100" alt="Javascript"/>
-                    <div className="carousel-caption d-md-block cover" >
+                    <div className="carousel-caption d-none d-md-block cover" >
                         <h5>Language of choice - Javascript</h5>
                         <p> As a sofware engineer, I am comfortable with other programming languages like Python and C#, however, my programming lannguage of choice is JavaScript.</p>
                             
@@ -37,21 +37,21 @@ class Expertise extends Component {
                     </div>
                     <div className="carousel-item">
                     <img src={rc} className="d-block w-100" alt="React JS"/>
-                    <div className="carousel-caption d-md-block d-sm-block cover">
+                    <div className="carousel-caption d-none d-md-block d-sm-block cover">
                         <h5>Frontend Development - React JS</h5>
                         <p>There is nothing better than being able to seperate your concerns while building complex user interfaces. I love React!</p>
                     </div>
                     </div>
                     <div className="carousel-item">
                     <img src={api} className="d-block w-100" alt="React JS"/>
-                    <div className="carousel-caption d-md-block d-sm-block cover">
+                    <div className="carousel-caption d-md-block d-none d-sm-block cover">
                         <h5>Appliaction Programming Interface -API </h5>
                         <p>What will we do without APIs? I am comfortable with RESTful APIs and can equally work with SOAP APIs when needed.</p>
                     </div>
                     </div>
                     <div className="carousel-item">
                     <img src={node} className="d-block w-100" alt="Node JS"/>
-                    <div className="carousel-caption d-md-block cover">
+                    <div className="carousel-caption d-none d-md-block cover">
                         <h5>Backend Development - Node JS</h5>
                         <p> I believe the beauty of JavaScript can be in seen our ability to use it from the Frontend to the Backend. I love JavaScript & the Node environment. </p>
                     </div>
